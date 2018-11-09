0e6388064b1895b9735cfd72d8113f6a6f7ae0c4
juancruzgardey <juancruzgardey@hotmail.com>
2018-11-09 17:19:20 -0300

    fixed LinkToTopRefactoring

--- src/content_scripts/views/RefactoringView.js
+++ src/content_scripts/views/RefactoringView.js
@@ -5,10 +5,17 @@ RefactoringView.prototype.render = function () {
     $("#refactorings-sidebar").append("<h4 style='text-align:center'>" + this.refactoring.constructor.getName() + "</h4>");
     $("#refactorings-sidebar").append("<div id='refactoring-form'></div>");
 
+    let url = document.location.href;
     var formGroup = $("<div class='refactorings-form-group'>")[0];
-    $(formGroup).append("<div class='refactoring-buttons'><button id='apply'>Apply</button></div>");
-    $(formGroup).append("<div class='refactoring-buttons'><button id='cancel'>Back</button></div>");
-    $("#refactorings-sidebar").append(formGroup);
+    $(formGroup).append("<label>URL for Refactoring</label>");
+    $(formGroup).append("<input type='text' id='url_for_instance' value='"+ url +"'/>");
+    $("#refactoring-form").append(formGroup);
+
+
+    var formGroup2 = $("<div class='refactorings-form-group'>")[0];
+    $(formGroup2).append("<div class='refactoring-buttons'><button id='apply'>Apply</button></div>");
+    $(formGroup2).append("<div class='refactoring-buttons'><button id='cancel'>Back</button></div>");
+    $("#refactorings-sidebar").append(formGroup2);
 
     var me = this;
     $("#apply").on("click", function () {
