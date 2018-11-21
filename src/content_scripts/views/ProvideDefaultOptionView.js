244264447e0eb1c2a4bfe4dfa106041058288410
juancruzgardey <juancruzgardey@hotmail.com>
2018-11-21 14:04:58 -0300

    added blank option to ProvideDefaultOption refactoring

--- src/content_scripts/views/ProvideDefaultOptionView.js
+++ src/content_scripts/views/ProvideDefaultOptionView.js
@@ -10,10 +10,16 @@ ProvideDefaultOptionView.prototype.render = function () {
 
     var options = $(this.refactoring.getElement()).find("option");
     $("#refactoring-form").append("<select type='text' id='refactoring_default_option'></select>");
+    $("#refactoring_default_option").append("<option value='-1'>No Default Option</option>");
     $("#refactoring_default_option").append(options.clone());
 };
 
 ProvideDefaultOptionView.prototype.setRefactoringArguments = function () {
-    var defaultOptionIndex = $('#refactoring_default_option').find("option:selected").index();
-    this.refactoring.setDefaultOption(defaultOptionIndex);
-}
+    if ($("#refactoring_default_option").val() == "-1") {
+        this.refactoring.setDefaultOption(-1);
+    }
+    else {
+        var defaultOptionIndex = $('#refactoring_default_option').find("option:selected").index();
+        this.refactoring.setDefaultOption(defaultOptionIndex);
+    }
+};
