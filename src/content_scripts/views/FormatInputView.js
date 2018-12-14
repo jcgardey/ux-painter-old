7158e8f808686adc7fed8a5490f50902fb9c5817
juancruzgardey <juancruzgardey@hotmail.com>
2018-12-14 12:11:33 -0300

    add description to FormatInput Refactoring and changed mask lib

--- src/content_scripts/views/FormatInputView.js
+++ src/content_scripts/views/FormatInputView.js
@@ -6,11 +6,13 @@ FormatInputView.prototype = new RefactoringOnElementView();
 
 FormatInputView.prototype.render = function () {
     RefactoringOnElementView.prototype.render.call(this);
-    $("#refactoring-form").append("<label>Format</label>");
-    $("#refactoring-form").append("<input type='text' id='input-format'/>");
-    $("#refactoring-form").append("<p>9: digit, </p>")
-
-
+    $("#refactoring-form").append("<label>Format String</label>");
+    $("#refactoring-form").append("<input type='text' id='input-format' placeholder='e.g. 9999-99-99 for Date'/>");
+    $("#refactoring-form").append("<p class='bold' style='margin: 10px 0px'>Options</p>");
+    $("#refactoring-form").append("<ul><li>0: {pattern: /\\d/}</li><li>9: {pattern: /\\d/, optional:true}</li>" +
+        "<li>#: {pattern: /\\d/, recursive: true}</li>" +
+        "<li>A: {pattern: /[a-zA-Z0-9]/}</li>" +
+        "<li>S: {pattern: /[a-zA-Z]/}</li>");
 };
 
 FormatInputView.prototype.setRefactoringArguments = function () {
