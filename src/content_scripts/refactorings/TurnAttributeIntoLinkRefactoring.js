2587547900a246cf1d4c9ac383b1bfcf3406f622
juancruzgardey <juancruzgardey@hotmail.com>
2019-01-04 11:21:14 -0300

    add img to the target elements of TurnAttributeIntoLink\

--- src/content_scripts/refactorings/TurnAttributeIntoLinkRefactoring.js
+++ src/content_scripts/refactorings/TurnAttributeIntoLinkRefactoring.js
@@ -34,5 +34,5 @@ TurnAttributeIntoLinkRefactoring.getView = function () {
 };
 
 TurnAttributeIntoLinkRefactoring.targetElements = function () {
-    return "p, span, h1, h2, h3, h4, h5, h6";
+    return "p, span, h1, h2, h3, h4, h5, h6, img";
 }
